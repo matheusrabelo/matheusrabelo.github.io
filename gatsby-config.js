1d02bd687b7b448f4c5269697373cf2e1a2640d4
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2020-10-27 10:21:38 -0300

    Add gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+    siteMetadata: {
+        title: 'Matheus Rabelo',
+        description: 'Personal website',
+        author: '@matheusrabelo',
+        email: 'matheusfreirerabelo[@]gmail[.]com',
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `matheusrabelo.github.io`,
+                short_name: `matheusrabelo`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`,
+            },
+        },
+        {
+            resolve: `gatsby-source-graphql`,
+            options: {
+                typeName: `GitHub`,
+                fieldName: `github`,
+                url: `https://api.github.com/graphql`,
+                headers: {
+                    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+                },
+            },
+        },
+        {
+            resolve: `gatsby-plugin-gtag`,
+            options: {
+                trackingId: `UA-96217823-1`,
+            },
+        },
+    ],
+};
