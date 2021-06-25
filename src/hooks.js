9d3f24cca6f5c1a624e0cb0f197a3ff36782e162
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2021-06-25 15:29:04 -0300

    Remove unused checking

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,7 @@ const isBrowser = typeof window !== 'undefined';
 
 export const useSendEvent = () => {
   if (isBrowser && window?.ga?.getAll) {
-    const tracker = window?.ga.getAll()[0];
+    const tracker = window.ga.getAll()[0];
 
     return (...args) => tracker.send('send', 'event', ...args);
   };
