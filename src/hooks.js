114e803028e2ef2260158ee9be0bfb6b2b2a4963
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2021-06-25 15:36:36 -0300

    Fix event sending

--- src/hooks.js
+++ src/hooks.js
@@ -4,7 +4,7 @@ export const useSendEvent = () => {
   if (isBrowser && window?.ga?.getAll) {
     const tracker = window.ga.getAll()[0];
 
-    return (...args) => tracker.send('send', 'event', ...args);
+    return (...args) => tracker.send('event', ...args);
   };
 
   return () => (...args) => console.log(JSON.stringify(args));
