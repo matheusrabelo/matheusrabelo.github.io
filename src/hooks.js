2c4f303d21693ef6d40491319a46a504bfc2037e
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2021-06-25 16:03:15 -0300

    Fix tracker

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 const log = (...args) => console.log(JSON.stringify(args));
 
 export const useSendEvent = () => ({ category, action, label }) => {
-  const tracker = window?.ga?.getAll()[0] || log;
+  const tracker = window?.ga?.getAll()[0] || { send: log };
   tracker.send({
     hitType: 'event',
     eventCategory: category,
