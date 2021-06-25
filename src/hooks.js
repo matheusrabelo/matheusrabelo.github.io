0e6233fc663a0fdce7ff2b05bf6493438f61ff06
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2021-06-25 15:28:20 -0300

    Fix event sending using GTM

--- src/hooks.js
+++ src/hooks.js
@@ -1,9 +1,11 @@
 const isBrowser = typeof window !== 'undefined';
 
-export const useSendEvent = (...args) => {
-  if (isBrowser && window?.ga?.send) {
-    return (...args) => window.ga.send('send', 'event', ...args);
+export const useSendEvent = () => {
+  if (isBrowser && window?.ga?.getAll) {
+    const tracker = window?.ga.getAll()[0];
+
+    return (...args) => tracker.send('send', 'event', ...args);
   };
 
-  return (...args) => console.log(JSON.stringify(args));
+  return () => (...args) => console.log(JSON.stringify(args));
 };
